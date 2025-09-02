01a93d68387f1d0bafc96d29ab957e8e7102d83c
pranav surwase <pranavsurwase93@gmail.com>
2025-09-02 19:01:22 +0530

    feat(job): show latest posted jobs first by ordering on posted_date DESC

--- backend/src/models/jobModel.js
+++ backend/src/models/jobModel.js
@@ -2,7 +2,7 @@ let db =require("../../db.js");
 
 exports.viewAllJobs = () => {
     return new Promise((resolve, reject) => {
-        db.query("select j.j_id,j.j_name,j.skills,j.location,j.hr_id,h.company_name FROM job j JOIN hr h ON  j.hr_id = h.hr_id; ", (err, result) => {
+        db.query("select j.j_id,j.j_name,j.skills,j.location,j.hr_id,h.company_name FROM job j JOIN hr h ON  j.hr_id = h.hr_id ORDER BY j.posted_date DESC; ", (err, result) => {
             if (err) {
                 reject(err);
             } else {
